2fa7e4f6f5e240c81e5a8f9863b34c7c8f0d520e
nikei065 <lehaidang12312312@gmail.com>
2024-11-22 17:25:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Nguyên BUổi chìu của tuii đó =))",
+    introTitle: 'Mẫn Yên "_'',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
